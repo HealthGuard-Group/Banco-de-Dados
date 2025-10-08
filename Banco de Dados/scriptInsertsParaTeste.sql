f7a7f45a81f767518b70a3703d962bd075ee7d5d
Arthur-Amaral-cloud <arthur.asilva@sptech.school>
2025-10-07 21:04:13 -0300

    Testes(ScriptInsert) - Adicionando os inserts para os teste do Login e Cadastro

--- Banco de Dados/scriptInsertsParaTeste.sql
+++ Banco de Dados/scriptInsertsParaTeste.sql
@@ -83,11 +83,50 @@ VALUES
 (1,1,7,7,'Médio',256,512),
 (1,1,7,7,'Grande',512,2000);
 */
+
+/*
 select * from MedicoesSelecionadas;
 select * from MetricaAlerta;
 SELECT max(valorMinimo) FROM MetricaAlerta WHERE fkDac = 1 AND fkMedicoesDisponiveis = 1;
 SELECT min(valorMaximo) FROM MetricaAlerta WHERE fkDac = 1 AND fkMedicoesDisponiveis = 1;
 SELECT idDac,fkUnidadeDeAtendimento FROM HealthGuard.Dac WHERE statusDac != 'Excluido' AND codigoValidacao = 'ABC123DEF456GHI78901';
+*/
+-- Inserts Para o cadastro e Login
+INSERT INTO healthguard.permissoes(nome,descricao) VALUES
+('Gestor TI',"Todas as funções do Analista TI e incluso visualizar logs de ações e adicionar funcionários"),
+('Analista TI','Pode visualizar a dashboard e adicionar uma nova máquina');
 
+INSERT INTO healthguard.CodigoValidacao 
+(fkUnidadeDeAtendimento, fkPermissoes, codigo, dataCriacao, dataExpiracao, statusCodigo)
+VALUES
+-- 1 - Expirado
+(1, 1, 'A1B2C3D4E5F6G7H', '2025-05-10 14:20:00', '2025-06-10 14:20:00', 'Expirado'),
+
+-- 2 - Expirado
+(1, 2, 'K9L8M7N6O5P4Q3R', '2025-04-01 09:00:00', '2025-05-01 09:00:00', 'Expirado'),
+
+-- 3 - Aceito
+(1, 1, 'Z1Y2X3W4V5U6T7S', '2025-07-12 11:35:00', '2025-08-12 11:35:00', 'Aceito'),
+
+-- 4 - Aceito
+(1, 2, 'P0O9I8U7Y6T5R4E', '2025-07-25 08:00:00', '2025-08-25 08:00:00', 'Aceito'),
+
+-- 5 - Pedente (data expirada → útil pra testar erro de lógica)
+(1, 1, 'M1N2B3V4C5X6Z7A', '2025-06-15 15:10:00', '2025-07-15 15:10:00', 'Pedente'),
+
+-- 6 - Pedente
+(1, 2, 'L0K9J8H7G6F5D4S', '2025-09-01 10:45:00', '2025-09-30 10:45:00', 'Pedente'),
+
+-- 7 - Expirado
+(1, 1, 'R3T4Y5U6I7O8P9Q', '2025-02-10 13:22:00', '2025-03-10 13:22:00', 'Expirado'),
+
+-- 8 - Aceito
+(1, 2, 'E1D2C3B4A5S6D7F', '2025-03-01 18:00:00', '2025-04-01 18:00:00', 'Aceito'),
+
+-- 9 - Pedente
+(1, 1, 'H1G2F3E4D5C6B7A', '2025-08-05 07:15:00', '2025-09-05 07:15:00', 'Pedente'),
+
+-- 10 - Expirado
+(1, 2, 'W9Q8E7R6T5Y4U3I', '2025-01-10 16:30:00', '2025-02-10 16:30:00', 'Expirado');
 
 
